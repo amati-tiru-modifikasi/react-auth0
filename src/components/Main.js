f36aaa701d7c373faf51e36d45941ae3b26f23e1
mdestafadilah <desta.08b@gmail.com>
2021-01-25 15:52:59 +0700

    Install auth0-react

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from '@chakra-ui/react';
 
 export default class Main extends Component {
-    render() {
-        return (
-            <div>
-               Main Komponen ... {this.props.name}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        Mau masuk ke Area Rahasia?
+        <br />
+        <Link color="teal.500" href="/secret">
+          Area Rahasia!
+        </Link>
+        <br />
+        Contoh Props -> {this.props.name}
+      </div>
+    );
+  }
 }
